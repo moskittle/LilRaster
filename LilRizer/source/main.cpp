d40af9e27eb46611b0c49c1e585261a950312615
shifeng <sfmarc1iang@gmail.com>
2019-08-26 15:35:37 +0800

    Remove multiplication and division operations in Draw loop

--- LilRizer/source/main.cpp
+++ LilRizer/source/main.cpp
@@ -6,10 +6,10 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
-struct Vec2
+struct Point
 {
-	Vec2() = default;
-	Vec2(int _x, int _y) : x(_x), y(_y) {}
+	Point() = default;
+	Point(int _x, int _y) : x(_x), y(_y) {}
 
 	int x = 0, y = 0;
 };
@@ -18,11 +18,12 @@ struct Line
 {
 	Line() = default;
 	Line(int x0, int y0, int x1, int y1)
+		: P0(x0, y0), P1(x1, y1) 
 	{
-		P0.x = x0; P0.y = y0; P1.x = x1; P1.y = y1;
 	}
 
-	bool IsSteep()
+	// Swap X and Y if the line is Steep to avoid holes
+	bool SwapXYifSteep()
 	{
 		int dx = abs(P1.x - P0.x), dy = abs(P1.y - P0.y);
 		bool bIsSteep = false;
@@ -35,23 +36,42 @@ struct Line
 		return bIsSteep;
 	}
 
-	void Draw(TGAImage &image, TGAColor color)
+	// Swap points to make sure P0 is on the left
+	void SwapP0toLeft()
 	{
-		bool bIsSteep = IsSteep();
-
-		int dx = P1.x - P0.x;
-		for (float x = P0.x, t = 0.0f, y = 0.0f; x < P1.x; ++x)
+		if (P0.x > P1.x)
 		{
-			// Draw
-			(bIsSteep) ? image.set(x, y, color) : image.set(y, x, color);
-
-			// Calculate y position
-			t = (x - P0.x) / dx;
-			y = (1 - t) * P0.y + t * P1.y;
+			std::swap(P0.x, P1.x);
+			std::swap(P0.y, P1.y);
 		}
 	}
 
-	Vec2 P0, P1;
+	void Draw(TGAImage &image, TGAColor color)
+	{
+		bool bIsSteep = SwapXYifSteep();
+
+		SwapP0toLeft();
+
+		int dx = P1.x - P0.x;
+		int dy = P1.y - P0.y;
+		float dt = std::abs(dy / static_cast<float>(dx));
+		float t = 0.0f;
+		for (int x = P0.x, y = P0.y; x < P1.x; ++x)
+		{
+			// Draw pixel
+			bIsSteep ? image.set(y, x, color) : image.set(x, y, color);
+
+			// Calculate y position
+			t += dt;
+			if (t > 0.5f)
+			{
+				y += (P1.y > P0.y ? 1 : -1);
+				--t;
+			}
+		}
+	}
+
+	Point P0, P1;
 };
 
 int main(/*int argc, char** argv*/)
@@ -64,12 +84,12 @@ int main(/*int argc, char** argv*/)
 	TGAImage image(400, 400, TGAImage::RGB);
 
 	timer.Start();
-	for (int i = 0; i <= 1000000; ++i)
+	for (int i = 0; i <= 100000; ++i)
 	{
 
 		Line line0(52, 80, 320, 160); line0.Draw(image, white);
 		Line line1(80, 52, 160, 320); line1.Draw(image, red);
-		Line line2(320, 160, 52, 80); line0.Draw(image, red);
+		Line line2(320, 160, 52, 80); line2.Draw(image, red);
 	}
 	timer.Stop();
 
